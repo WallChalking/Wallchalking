202e9293078043c941bc0a42a9061733985494ea
Mohsin <mohsin@mohsin-laptop.(none)>
2013-09-07 01:30:27 +0500

    InProgress

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120418171112) do
+ActiveRecord::Schema.define(:version => 20130904185411) do
+
+  create_table "authentications", :force => true do |t|
+    t.string   "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "token"
+    t.string   "token_secret"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "roles", :force => true do |t|
     t.string   "name"
